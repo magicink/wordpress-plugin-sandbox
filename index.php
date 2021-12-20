ff7f42eb66ca3db63bbdf06e21b7c41d957ae9ce
Brandon Tom <magicink@gmail.com>
2021-12-20 06:23:46 -0800

    Updated plugin config

--- index.php
+++ index.php
@@ -9,17 +9,6 @@ if (!class_exists(PluginSandbox::class)) {
     public function __construct()
     {
       remove_filter('the_content', 'wpautop');
-      add_filter( 'pods_register_post_type_mycpt', 'add_pods_graphql_support' );
-      add_filter( 'pods_register_taxonomy_mytax', 'add_pods_graphql_support' );
-
-      function add_pods_graphql_support( $options ) {
-
-        $options['show_in_graphql'] = true;
-        $options['graphql_single_name'] = $options['labels']['name'];
-        $options['graphql_plural_name'] = $options['labels']['singular_name'];
-
-        return $options;
-      }
     }
   }
 
